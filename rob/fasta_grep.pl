8b9a16b0855879274399934d576018e3fe791b96
Robert Buels <rmb32@cornell.edu>
2010-07-01 19:03:09 -0400

    another old script from sgn-tools

---
+++ rob/fasta_grep.pl
@@ -0,0 +1,75 @@
+#!/usr/bin/perl
+
+eval 'exec /usr/bin/perl -w -S $0 ${1+"$@"}'
+    if 0; # not running under some shell
+
+use strict;
+use warnings;
+
+use English;
+use Carp;
+use FindBin;
+
+use Getopt::Std;
+use Pod::Usage;
+
+#use Data::Dumper;
+
+our %opt;
+getopts('',\%opt) or pod2usage(1);
+my $pat = shift @ARGV;
+$pat = qr/$pat/;
+
+
+$/ = "\n>";
+
+my $first = <>;
+chomp $first;
+print "$first\n" if $first =~ $pat;
+
+while(my $seq = <>) {
+  chomp $seq;
+  print ">$seq\n" if $seq =~ $pat;
+}
+
+
+__END__
+
+=head1 NAME
+
+fasta_grep.pl - like 'grep', but for use on fasta-format sequence
+files.
+
+=head1 DESCRIPTION
+
+Matches the perl regexp given on the command line to the
+ENTIRE fasta record (ident, defline, sequence) and prints it if it
+matches.
+
+Idiosyncracy: Puts an extra newline at the end of the file if the last
+seq in your input matches.
+
+=head1 SYNOPSIS
+
+  fasta_grep.pl  perl_regular_expression  list_of_files
+
+  Options:
+
+    none yet
+
+=head1 MAINTAINER
+
+Robert Buels
+
+=head1 AUTHOR(S)
+
+Robert Buels, E<lt>rmb32@cornell.eduE<gt>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Boyce Thompson Institute for Plant Research
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
