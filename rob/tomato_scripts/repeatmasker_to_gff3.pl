bab9d61d3f88c32b667a651f1149bb6e3aad1e70
Robert Buels <rmb32@cornell.edu>
2011-03-08 18:15:53 -0500

    add one-off script for converting repeatmasker to gff3

---
+++ rob/tomato_scripts/repeatmasker_to_gff3.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Bio::FeatureIO;
+use Bio::Tools::RepeatMasker;
+
+my $parser = Bio::Tools::RepeatMasker->new(-file => @ARGV);
+my $g = Bio::FeatureIO->new(
+    -format => 'gff',
+    -version => 3,
+    -fh => \*STDOUT,
+  );
+while( my $result = $parser->next_result ) {
+    my $f = $result->feature1;
+
+    # normalize the primary tags;
+    my $type = $f->primary_tag;
+    $f->primary_tag( 'repeat_region' );
+    $f->add_tag_value('repeat_class',$type);
+
+    #fix the Target attributes
+    my @tgt = $f->remove_tag('Target');
+    if( $tgt[1] > $tgt[2] ) {
+        @tgt[1,2] = @tgt[2,1];
+    }
+    $f->add_tag_value( 'Target', join ' ', @tgt, '+' );
+
+    $g->write_feature( $f );
+}
+
