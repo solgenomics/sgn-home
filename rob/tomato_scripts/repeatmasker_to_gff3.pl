10bf006cba4023b990cadc4fee2fb8ec9d49a6f2
Robert Buels <rmb32@cornell.edu>
2011-03-09 10:19:21 -0500

    make repeatmasker->gff3 converter take multiple files

--- rob/tomato_scripts/repeatmasker_to_gff3.pl
+++ rob/tomato_scripts/repeatmasker_to_gff3.pl
@@ -5,27 +5,29 @@ use warnings;
 use Bio::FeatureIO;
 use Bio::Tools::RepeatMasker;
 
-my $parser = Bio::Tools::RepeatMasker->new(-file => @ARGV);
 my $g = Bio::FeatureIO->new(
     -format => 'gff',
     -version => 3,
     -fh => \*STDOUT,
   );
-while( my $result = $parser->next_result ) {
-    my $f = $result->feature1;
 
-    # normalize the primary tags;
-    my $type = $f->primary_tag;
-    $f->primary_tag( 'repeat_region' );
-    $f->add_tag_value('repeat_class',$type);
+for my $file (@ARGV) {
+    my $parser = Bio::Tools::RepeatMasker->new(-file => $file);
+    while ( my $result = $parser->next_result ) {
+        my $f = $result->feature1;
 
-    #fix the Target attributes
-    my @tgt = $f->remove_tag('Target');
-    if( $tgt[1] > $tgt[2] ) {
-        @tgt[1,2] = @tgt[2,1];
+        # normalize the primary tags;
+        my $type = $f->primary_tag;
+        $f->primary_tag( 'repeat_region' );
+        $f->add_tag_value('repeat_class',$type);
+
+        #fix the Target attributes
+        my @tgt = $f->remove_tag('Target');
+        if ( $tgt[1] > $tgt[2] ) {
+            @tgt[1,2] = @tgt[2,1];
+        }
+        $f->add_tag_value( 'Target', join ' ', @tgt, '+' );
+
+        $g->write_feature( $f );
     }
-    $f->add_tag_value( 'Target', join ' ', @tgt, '+' );
-
-    $g->write_feature( $f );
 }
-
