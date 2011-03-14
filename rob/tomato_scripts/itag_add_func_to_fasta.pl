b2bf6f0bfadc6ce47080ff451cea668b8c1f4b73
Robert Buels <rmb32@cornell.edu>
2011-03-14 01:19:39 -0400

    script to add functional annotation data (input in idiosyncratic csv format) to fasta deflines

---
+++ rob/tomato_scripts/itag_add_func_to_fasta.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use autodie ':all';
+
+my ( $specific_csv, $general_csv, $descriptions_file, $fasta_file ) = @ARGV;
+
+my %func;
+# hash of protein_id => {
+#   specific => [ go, go, ...],
+#   general  => [ go, go, ...],
+#   combined => [ go, go, ...],
+#   description => 'functional description',
+# }
+
+# load the go terms
+load_kklee_go_file( $specific_csv, \%func, 'specific' );
+load_kklee_go_file( $general_csv,  \%func, 'general'  );
+combine_go( \%func );
+
+# load the human-readable description
+load_descriptions( $descriptions_file, \%func );
+
+
+open my $fasta_in, '<', $fasta_file;
+while( <$fasta_in> ) {
+    if( my ($id) = /^>\s*(\S+)/ ) {
+        $id =~ s/\.\d//;
+        if( my $terms = $func{ $id }{combined} ) {
+            chomp;
+            $_ .= " go_terms:".join(',', @$terms )."\n";
+        }
+        if( my $desc = $func{ $id }{description} ) {
+            chomp;
+            $desc =~ s/"/'\"'/ge;
+            $_ .= qq| functional_description:"$desc"\n|;
+        }
+    }
+    print;
+}
+
+exit;
+
+######### subs #########
+
+sub load_descriptions {
+    my ( $desc_file, $func ) = @_;
+    open my $desc, '<', $desc_file;
+    while( my $line = <$desc> ) {
+        chomp $line;
+        my @cols = split /";"/, $line;
+        my $prot_id = $cols[1];
+        $prot_id =~ s/\.\d//;
+        my $desc = $cols[4];
+        $func{$prot_id}{description} = $desc;
+    }
+
+}
+
+sub combine_go {
+    my ( $go ) = @_;
+
+    for my $prot ( keys %$go ) {
+        my $rec = $go->{$prot};
+        $rec->{combined} = $rec->{specific} || $rec->{general};
+    }
+}
+
+sub load_kklee_go_file {
+    my ( $file, $go, $type ) = @_;
+
+    open my $fh, '<', $file;
+    while ( my $line = <$fh> ) {
+        chomp $line;
+        next if $line =~ /No GOs/;
+        my ( $protein_id, @terms) = split /\s+/, $line;
+        $protein_id =~ s/\.\d//;
+        s/,+$// for @terms;
+        push @{ $go->{$protein_id}{$type} ||= [] }, @terms
+    }
+}
