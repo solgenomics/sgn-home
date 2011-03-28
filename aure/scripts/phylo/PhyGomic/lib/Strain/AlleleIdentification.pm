176a2466c68bba440ae8ab38d859163060f14692
Aureliano Bombarely <aubombarely@gmail.com>
2011-03-28 09:24:28 -0400

    A module to identify alleles in a tree.

---
+++ aure/scripts/phylo/PhyGomic/lib/Strain/AlleleIdentification.pm
@@ -0,0 +1,293 @@
+
+package Strain::AlleleIdentification;
+
+use strict;
+use warnings;
+use autodie;
+
+use Carp qw| croak cluck |;
+
+## To export some functions
+
+use Exporter qw( import );
+
+our @EXPORT_OK = qw( identify_alleles );
+
+
+###############
+### PERLDOC ###
+###############
+
+=head1 NAME
+
+Strain::AlleleIdentification.pm
+An object to identify alleles using tree structure.
+
+=cut
+
+our $VERSION = '0.01';
+$VERSION = eval $VERSION;
+
+=head1 SYNOPSIS
+
+  use Strain::AlleleIdentification qw/ identify_alleles /;
+
+  my %allele_args = (
+    strains      => \%strains,
+    alignment    => $alignment,
+    tree         => $tree,
+    parents      => \%parents,
+    target       => $target,
+    filter       => \%filter,
+  );
+
+  my %alleles = identify_alleles(\%allele_args);
+
+
+=head1 DESCRIPTION
+
+ Function to identify the alleles in a polyploid/heterozigous species 
+ knowing its parents.
+
+ Steps:
+              |1
+       -------+-------
+       |2            |3
+    ---+---       ---+---
+    |     |       |     |
+    T1    A       T2    B
+
+  1) Get the leaves in the tree of the target specie. (T1 and T2)
+  
+  2) Get the ancestor node for these leaves and get all the leaves for
+     this ancestor (T1 => 2 => T1,A and T2 => 3 => T2,B),
+ 
+  3) Compare the leaves under the same node-ancestor. If there are
+     just only one parent and one target it will identify that target
+     as the allele that comes from the parent (T1-A and T2-B)
+
+  4) Calculate the identity and the possible SNPs between alleles and
+     parents.
+
+  5) Apply the filter according the following parameters:
+     i)   alignment length
+     ii)  alignment identity percentage
+
+  If the allele is in another branch ir will not be identify as allele
+
+                 |1
+           ------+------                   only T3 will be identify as 
+           |2          |                   allele of B
+       ----+----       |
+       |3      |       |4
+    ---+---    |    ---+---
+    |     |    |    |     |
+    T1    T2   A    T3    B
+
+  
+  
+
+=head1 AUTHOR
+
+Aureliano Bombarely <ab782@cornell.edu>
+
+
+=head1 CLASS METHODS
+
+The following class methods are implemented:
+
+=cut 
+
+
+
+############################
+### GENERAL CONSTRUCTORS ###
+############################
+
+=head2 identify_alelles
+
+  Usage: my %alleles = identify_alleles(\%allele_args);
+
+  Desc: Analyze a phylogenetic tree with some members and its parents or
+        close related ancestors (see description for more details)
+
+  Ret: A hash with key=member_id, value=allele_name
+
+  Args: A hash reference with the following key-value pairs: 
+         strains      => a hashref. with key=member_id, value=strains
+         alignment    => $alignment, a Bio::SimpleAlign object
+         tree         => $tree, a Bio::Tree::Tree object
+         parents      => a hashref with key=parent/ancestors strain
+                                        value=allele name
+         target       => $target, a strain-target name
+         filter       => a hashref. with key=(length|identity), 
+                                         value=cutoff value
+        
+  Side_Effects: Die if the no argument is used or it is not a hash ref.
+                Die if no strains,alignment,tree,parents or target argument
+                is used.
+                Die if the format of the argument is not right.
+                Return an empty hash if not alleles were identified.
+
+  Example: my %alleles = identify_alleles(\%allele_args);
+
+=cut
+
+sub identify_alleles {
+    my $args_href = shift ||
+	croak("ERROR: No arguments were supplied to identify_alleles()");
+
+    my %args = ();
+    if (ref($args_href) ne 'HASH') {
+	croak("ERROR: Argument supplied to identify_alleles() isnt a HASHREF.");
+    }
+    else {
+	%args = %{$args_href};
+    }
+
+    ## Check that the arguments have the right format
+
+    my %perms = (
+	strains   => 'HASH',
+	alignment => 'Bio::SimpleAlign',
+	tree      => 'Bio::Tree::Tree',
+	parents   => 'HASH',
+	target    => undef,
+	filter    => 'HASH',
+	);
+
+    foreach my $arg (keys %args) {
+	unless (exists $perms{$arg}) {
+	    croak("ERROR: $arg is a non-permited argum. for identify_alleles");
+	}
+	else {
+	    if (defined $perms{$arg} && ref($args{$arg}) ne $perms{$arg}) {
+		croak("ERROR: $arg doesnt have right value identify_alleles()");
+	    } 
+	}
+    }
+
+    ## Check the mandatory arguments (everything except filter)
+    
+    foreach my $marg (keys %perms) {
+	
+	if ($marg ne 'filter') {
+	    unless (exists $args{$marg}) {
+		croak("ERROR: Mandatory argument $marg wasnt used as argument");
+	    }
+	}
+    }
+
+    ## Declare possible alleles hash
+
+    my %pos_alleles = ();
+    my %pos_alleles_neigh = ();
+
+    ## Everything has been checked, now it will start the allele identification
+
+    my %strains = %{$args{strains}};
+    my %parents = %{$args{parents}};
+    
+    if (scalar(keys %parents) < 2) {
+	croak("ERROR: Less than one parent was specified as argument.");
+    }
+    
+    my $tree = $args{tree};
+
+    ## 1) Get the ancestors nodes for all the target leaves
+    ##    Get the leaves for these nodes and check if they are parents
+   
+    my @target_members = ();
+  
+    foreach my $node ( $tree->get_nodes() ) {
+	
+	if ($node->is_Leaf()) {
+
+	    my $node_id = $node->id();
+	    if (exists $strains{$node_id}) {
+		if ($strains{$node_id} eq $args{target}) {
+
+		    push @target_members, $node_id;
+
+		    my $ancestor = $node->ancestor();
+		    my $parents_match = 0;
+		    my $parent_allele;
+		    
+		    foreach my $descent ($ancestor->get_all_Descendents()) {
+			
+			if ($descent->is_Leaf()) {
+			    
+			    if (exists $strains{$descent->id()}) {
+
+				my $desc_strain = $strains{$descent->id()};
+				if (exists $parents{$desc_strain}) {
+				    $parents_match++;
+				    $parent_allele = $descent->id();
+				}
+			    }
+			}
+		    }
+
+		    if ($parents_match == 1) { ## Add only of one of the parents
+                                               ## was found
+
+			my $parentallele = $parents{$strains{$parent_allele}};
+			$pos_alleles{$node_id} = $parentallele;
+			$pos_alleles_neigh{$node_id} =  $parent_allele;
+		    }
+		}
+	    }
+	}
+    }
+
+    my %alleles = ();
+
+    ## 2) The alleles should be identified, so now it will get some 
+    ##    values from the alignment
+
+    my $align = $args{alignment};
+    
+    if (defined $args{filter}) {
+    
+	foreach my $target (keys %pos_alleles) {
+
+	    my $newalign = Bio::SimpleAlign->new();
+	    
+	    my $seq = $align->get_seq_by_id($target);
+	    unless (defined $seq) {
+		croak("ERROR: No seq was found in $align for id=$target.")
+	    }
+	    else {
+		$newalign->add_seq($seq);
+	    }
+	    
+	    my $par =  $align->get_seq_by_id($pos_alleles_neigh{$target});
+	    unless (defined $par) {
+		croak("ERROR: No seq was found id=$pos_alleles_neigh{$target}.")
+	    }
+	    else {
+		$newalign->add_seq($par);
+	    }
+
+	    if ($args{filter} eq 'length') {
+		if ($newalign->length >= $args{filter}->{length}) {
+		    $alleles{$target} = $pos_alleles{$target};
+		}
+	    }
+	    elsif ($args{filter} eq 'identity') {
+		if ($newalign->percentage_identity >= $args{filter}->{length}){
+		    $alleles{$target} = $pos_alleles{$target};
+		}
+	    }
+	}
+    }
+    else {
+	%alleles = %pos_alleles;
+    }
+     
+    return %alleles;
+}
+
+####
+1; #
+####
---
+++ aure/scripts/phylo/PhyGomic/t/strain_alleleidentification.t
@@ -0,0 +1,208 @@
+#!/usr/bin/perl
+
+=head1 NAME
+
+  strain_alleleidentification.t
+  A piece of code to test the Strain::AlleleIdentification module used 
+  for PhylGomic pipeline
+
+=cut
+
+=head1 SYNOPSIS
+
+ perl strain_alleleidentification.t
+ prove strain_alleleidentification.t
+
+=head1 DESCRIPTION
+
+ Test Strain::AlleleIdentification module used by PhylGomic pipeline.
+
+=cut
+
+=head1 AUTHORS
+
+ Aureliano Bombarely Gomez
+ (ab782@cornell.edu)
+
+=cut
+
+use strict;
+use warnings;
+use autodie;
+
+use Data::Dumper;
+use Test::More tests => 20;
+use Test::Exception;
+
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+
+## TEST 1
+
+BEGIN {
+    use_ok('Strain::AlleleIdentification', qw( identify_alleles ));
+    use_ok('Bio::LocatableSeq');
+    use_ok('Bio::SimpleAlign');
+    use_ok('Bio::Tree::Node');
+    use_ok('Bio::Tree::Tree');
+}
+
+## Prepare the data
+
+## Alignment
+
+my %seqs = (
+    sq1 => 'ATGCGCCATGACCACGATGGGCAGGATAAATCGGGATAGATAGTAGCCAGCATGACGTGGCAGTAG',
+    sq2 => 'ATGCGCCACGACCACGATGGGCAGGATATATCGGGATAGACCGTAG-CAGCATGACGCGGCAGTAG',
+    sq3 => 'ATGCCGGATGACCAGGATGGGCGGGATAAATCGAGATAGTTACTACCCAGCATGACCTCCCAGTAG',
+    sq4 => 'ATGCCGGATGACCAGGATGCGCGGGATAAATCGAGATAGTTACAAC-CAGCATGACCACCCAGTAG',
+    sq5 => 'ATGCCCGATGACCAGGATCCGCGGGATATATCGAGATAGATACAAC-CAGCATGACGAGGCAGTAG',
+    sq6 => 'ATGCCGGACGACCAGGATGCGCGGGATAAAACGAGATAGTTACAACCCAGCAAGACCACCCAGTAG',
+    );
+
+my @locseqs = ();
+foreach my $seqname (sort keys %seqs) {
+    my $end = 66;
+    if ($seqs{$seqname} =~ m/-/) {
+	$end--;
+    }
+
+    push @locseqs, Bio::LocatableSeq->new( -id    => $seqname, 
+					   -seq   => $seqs{$seqname},
+					   -start => 1,
+					   -end   => $end,
+	);
+}
+
+my $align = Bio::SimpleAlign->new(-seqs => \@locseqs);
+
+## Tree                              +----------- sq1 (A)
+##                                   |
+## +---------------------------------+ 
+## |                                 |
+## +                                 +---------------- sq2 (T)
+## |   +----------- sq5 (C)
+## |   |
+## +---+                 +------------ sq3 (T) 
+##     |                 |
+##     +-----------------+        +--- sq4 (B)
+##                       |        |
+##                       +--------+
+##                                |
+##                                +--------------- sq6 (T)
+
+my $node1 = Bio::Tree::Node->new(-id => 'sq1', -branch_length => 0.02413);
+my $node2 = Bio::Tree::Node->new(-id => 'sq2', -branch_length => 0.05280);
+my $node3 = Bio::Tree::Node->new(-id => 'sq3', -branch_length => 0.02552);
+my $node4 = Bio::Tree::Node->new(-id => 'sq4', -branch_length => 0.00350);
+my $node5 = Bio::Tree::Node->new(-id => 'sq5', -branch_length => 0.04289);
+my $node6 = Bio::Tree::Node->new(-id => 'sq6', -branch_length => 0.04266);
+
+my $node7 = Bio::Tree::Node->new( -descendents   => [$node1, $node2], 
+				  -branch_length => 0.08957 );
+my $node8 = Bio::Tree::Node->new( -descendents   => [$node4, $node6], 
+				  -branch_length => 0.01993 );
+my $node9 = Bio::Tree::Node->new( -descendents   => [$node3, $node8], 
+				  -branch_length => 0.05839 );
+my $node10 = Bio::Tree::Node->new( -descendents   => [$node5, $node9], 
+	 			   -branch_length => 0.02139 );
+
+my $root = Bio::Tree::Node->new( -descendents => [$node7, $node10]);
+
+my $tree = Bio::Tree::Tree->new( -root => $root );
+
+
+## Strains
+
+my %strains = ( sq1 => 'A', 
+		sq2 => 'T', 
+		sq3 => 'T', 
+		sq4 => 'B', 
+		sq5 => 'C', 
+		sq6 => 'T');
+
+
+## Test the function
+
+my %allele_args = ( strains   => \%strains,
+		    alignment => $align,
+		    tree      => $tree,
+		    target    => 'T',
+		    parents   => { 'A' => 'T-a', 'B' => 'T-b'},
+    );
+
+my %alleles = identify_alleles(\%allele_args);
+
+
+## Test
+
+is(scalar(keys %alleles), 3,
+    "testing identify_alleles, checking number of alleles")
+    or diag("Looks like this has failed");
+
+is($alleles{sq1}, undef,
+    "testing identify_alleles, checking allele for one of the parents(undef)")
+    or diag("Looks like this has failed");
+
+is($alleles{sq2}, 'T-a',
+    "testing identify_alleles, checking allele for one of the targets (T-a)")
+    or diag("Looks like this has failed");
+
+is($alleles{sq3}, 'T-b',
+    "testing identify_alleles, checking allele for one of the targets (T-b)")
+    or diag("Looks like this has failed");
+
+is($alleles{sq4}, undef,
+    "testing identify_alleles, checking allele for the other parent (undef)")
+    or diag("Looks like this has failed");
+
+is($alleles{sq5}, undef,
+    "testing identify_alleles, checking allele for one of the targets (undef)")
+    or diag("Looks like this has failed");
+
+is($alleles{sq6}, 'T-b',
+    "testing identify_alleles, checking allele for one of the targets (T-b)")
+    or diag("Looks like this has failed");
+
+throws_ok { identify_alleles() } qr/ERROR: No arguments/,
+    "TESTING DIE ERROR: when no argument are used for identify_alleles()";
+
+throws_ok { identify_alleles([]) } qr/ERROR: Argument supplied/,
+    "TESTING DIE ERROR: when arg. used for identify_alleles() isnt hashref";
+
+throws_ok { identify_alleles({ fake => 1}) } qr/ERROR: fake is a non-permited/,
+    "TESTING DIE ERROR: when arg. used for identify_alleles() isnt permited";
+
+throws_ok { identify_alleles({ strains => 1})} qr/ERROR: strains doesnt have/,
+    "TESTING DIE ERROR: when arg. used doesnt have right value";
+
+throws_ok { identify_alleles({})} qr/ERROR: Mandatory argument/,
+    "TESTING DIE ERROR: when mandatory argument is not used";
+
+my %ff_allele_args = %allele_args;
+$ff_allele_args{parents} = { 'A' => 'T-a'};
+
+throws_ok { identify_alleles(\%ff_allele_args)} qr/ERROR: Less than one parent/,
+    "TESTING DIE ERROR: when less tha one parent was used";
+
+$node2->id('seq2');
+$strains{seq2} = 'T';
+$allele_args{filter} = {identity => 60};
+
+throws_ok { identify_alleles(\%allele_args)} qr/ERROR: No seq was found/,
+    "TESTING DIE ERROR: when target seqids in the tree are not in the align";
+
+$node2->id('sq2');
+
+$node1->id('seq1');
+$strains{seq1} = 'A';
+
+throws_ok { identify_alleles(\%allele_args)} qr/ERROR: No seq was found/,
+    "TESTING DIE ERROR: when parents seqids in the tree are not in the align";
+
+$node1->id('sq1');
+
+
+####
+1; #
+####
