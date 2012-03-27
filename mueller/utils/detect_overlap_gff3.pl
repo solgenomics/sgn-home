62299aba0bd2f175ccaa28019cbb05a5f11223de
mueller <lam87@cornell.edu>
2012-03-27 13:57:02 -0400

    generate better stats.

--- mueller/utils/detect_overlap_gff3.pl
+++ mueller/utils/detect_overlap_gff3.pl
@@ -5,7 +5,7 @@ detect_overlap_gff3.pl - detect overlapping features in two gff3 files
 
 =head1 SYNOPSIS
 
-detect_overlap_gff3.pl reference.gff mapped.gff
+detect_overlap_gff3.pl -t [feature_type] reference.gff mapped.gff
 
 Output:
 
@@ -30,8 +30,11 @@ use Getopt::Std;
 
 our %args;
 
-getopts('t:', \%args);
+getopts('t:v', \%args);
 
+if (!defined($args{t})) { 
+    die "Need -t for feature type.";
+}
 
 my $genome_gff = shift;
 my $marker_gff = shift;
@@ -39,48 +42,75 @@ my $marker_gff = shift;
 my @genome_gff = read_file($genome_gff);
 
 my %features =();
+my $feature_count =0;
+my %features_by_ref;
 
-my $count =0;
+print STDERR "Processing...\n";
+
 foreach my $g (@genome_gff) { 
     chomp($g);
     my ($ref, $source, $type, $start, $end, $dot, $dir, $frame, $desc) = split /\t/, $g;
 
     if ($type eq $args{t}) { 
 	$desc =~ s/.*ID=[.*?:]*(.*?)\.\d+;.*/$1/;
-	
-    
 	$features{$desc}->{start} = $start;
 	$features{$desc}->{end} = $end;
 	$features{$desc}->{ref} = $ref;
-	$count++;
-    }
-    
+	$feature_count++;
+	$features_by_ref{$ref}++;
+    }    
 }
-print STDERR "Parsed $count genes.\n";
+
 my @marker_gff = read_file($marker_gff);
 my $total_covered =0;
-my $feature_count = 0;
+my $total_feature_count =0;
+my %total_unique_coverage;
+my %total_unique_count;
 
 foreach my $m (@marker_gff) { 
     chomp($m);
     my ($ref, $source, $type, $start, $end, $dot, $dir, $frame, $desc) = split /\t/, $m;
-    
-    $desc =~ s/.*ID=(.*?);.*/$1/;
+
+    if (defined($desc)) { 
+	$desc =~ s/.*ID=(.*?);.*/$1/;
+    }
 
     foreach my $k (keys(%features)) { 
-	if ($ref eq $features{$k}->{ref}) { # on same chr?
+	if ($ref eq $features{$k}->{ref}) { # on same ref (chr)?
 	    my $overlap = detect_overlap($start, $end, $features{$k}->{start}, $features{$k}->{end});
 	    
 	    if ($overlap) { 
-		print STDERR "detected overlap: $k $desc               \r";
-		print "$k\t$desc\n";
-		$total_covered += $features{$k}->{end} - $features{$k}->{start};
-		$feature_count++;
+		#print STDERR "detected overlap: $k $desc               \r";
+		if ($args{v}) { print "$k\t$desc\n"; }
+		my $coverage = $features{$k}->{end} - $features{$k}->{start};
+		$total_covered += $coverage;
+		$total_feature_count++;
+		$total_unique_coverage{$ref}->{$k} += $coverage;
+		$total_unique_count{$ref}->{$k}++;
 	    }
 	}
     }
 }
-print STDERR "Done. A total of $feature_count features had overlap; the features totalled $total_covered basepairs.\n";
+
+print STDERR qq { Parsed $feature_count features of type $args{t} in file $genome_gff.\n };
+
+print STDERR "$total_feature_count features covered $total_covered bp\n";
+#printf STDERR "10%s" REFERENCE, "10%s" "#FEATURES", "10%s" "BASEPAIRS\n";
+
+foreach my $ref (sort(keys(%total_unique_count))) { 
+    my $total_unique_features = scalar(keys(%{$total_unique_count{$ref}}));
+    my $total_unique_coverage = 0;
+    foreach my $k (keys(%{$total_unique_coverage{$ref}})) { 
+	if (defined($total_unique_coverage{$ref}->{$k}) ) { 
+	    $total_unique_coverage += $total_unique_coverage{$ref}->{$k};
+	}
+    }
+
+    printf STDERR "%10s %10d %10d %10d", $ref, $features_by_ref{$ref}, $total_unique_features, $total_unique_coverage;
+    print STDERR "\n";
+
+
+}
 
 sub detect_overlap { 
     my $start = shift;
